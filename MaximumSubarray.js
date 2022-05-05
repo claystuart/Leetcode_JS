96cd332d6b9e7554fc99166487646eabd8396a1b
Clay Stuart <clay.stuart@gmail.com>
2022-05-05 16:29:47 -0400

    Created MaxumumSubarray.js (Leet #53).

---
+++ MaximumSubarray.js
@@ -0,0 +1,12 @@
+"use strict"
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+    
+};
+
+console.log('Correct(6) => ' + maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+console.log('Correct(1) => ' + maxSubArray([1]));
+console.log('Correct(23) => ' + maxSubArray([5,4,-1,7,8]));
