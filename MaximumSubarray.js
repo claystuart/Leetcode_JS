cbe3592607520e8b6f054accdd1eec253d0bbd25
Clay Stuart <clay.stuart@gmail.com>
2022-05-06 15:15:05 -0400

    Exploring a non-brute force strategy to find the max subarray.

--- MaximumSubarray.js
+++ MaximumSubarray.js
@@ -3,19 +3,17 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
+    {//Pseudocode:
     //Goal: find the subarray with the largest sum
-    //Pseudocode: loop through every possible combination, searching for the largest sum
-    //create largestSum variable to track return value; initialize to -Infinity
-
-    //outside loop, loops through each index in the array
-    //inside loop, starts with outside loop index and sums every combination to the end
-        //compare each iteration against largestSum. If greater, assign to largestSum
-    //return largestSum
-
+    //Create leftPost and rightPost
+    //loop through list, summing elements from leftPost and resetting leftPost whenever a 
+    //new element is largest than previous sum. Then we just keep iterating through, looking for 
+    //rightPost that gives us the largest sum.
+    }
     
 };
 
-console.log('Correct(6) => ' + maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
-console.log('Correct(1) => ' + maxSubArray([1]));
-console.log('Correct(23) => ' + maxSubArray([5,4,-1,7,8]));
+// console.log('Correct(6) => ' + maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+// console.log('Correct(1) => ' + maxSubArray([1]));
+// console.log('Correct(23) => ' + maxSubArray([5, 4, -1, 7, 8]));
