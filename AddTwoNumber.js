f5d60cf1c38dcb4f93b935d56e73a8d4199cb6d3
Clay Stuart <clay.stuart@gmail.com>
2022-05-08 21:17:03 -0400

    Implemented AddTwoNumbers.js -- passed tests 1565 out of 1568.

--- AddTwoNumber.js
+++ AddTwoNumber.js
@@ -1,15 +1,73 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+
+//Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
- var addTwoNumbers = function(l1, l2) {
-    
-};
+var addTwoNumbers = function (l1, l2) {
+    let num1 = 0, num2 = 0;
+    let multiple = 1;
+
+    while (l1 != null) {
+        num1 = multiple * l1.val + num1;
+        multiple *= 10;
+        l1 = l1.next;
+    }
+
+    multiple = 1;
+    while (l2 != null) {
+        num2 = multiple * l2.val + num2;
+        multiple *= 10;
+        l2 = l2.next;
+    }
+    let sum = num1 + num2;
+    //good so far
+
+    if (sum === 0)
+        return new ListNode(0);
+
+    function makeList(number) {
+        if (number > 0)
+            return new ListNode(number % 10, makeList(Math.floor(number / 10)));
+        else
+            return;
+    }
+
+    let head = makeList(sum);
+
+    return head;
+}
+
+function test() {
+    // let a = new ListNode(2, new ListNode(4, new ListNode(3)));
+    // let b = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+    // let list = addTwoNumbers(new ListNode([0]), new ListNode([0]));
+    // printList(list);
+
+    let d = arrayToList([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]);  
+    let e = arrayToList([5,6,4]);
+    let list = addTwoNumbers(d,e);
+    printList(list);
+}
+
+test();
+
+function printList(list) {
+    while (list != null) {
+        console.log(list.val);
+        list = list.next;
+    }
+}
+
+function arrayToList(array) {
+    if (array.length > 0)
+        return new ListNode(array.shift(), arrayToList(array));
+}
+
