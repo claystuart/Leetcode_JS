b7d7f844cfde7bf172e0e3dfbbbbbfc8f033fc1f
Clay Stuart <clay.stuart@gmail.com>
2022-05-13 18:28:32 -0400

    Reimplemented  LongestSubstring.js using better algorithm to make much fast.

--- LongestSubstring.js
+++ LongestSubstring.js
@@ -3,39 +3,39 @@
  * @return {number}
  */
 /* Details:
-Runtime: 279 ms, faster than 26.48% of JavaScript online submissions 
-    for Longest Substring Without Repeating Characters.
-Memory Usage: 48.6 MB, less than 42.10% of JavaScript online submissions 
-    for Longest Substring Without Repeating Characters.
+Runtime: 98 ms, faster than 83.24% of JavaScript online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 45 MB, less than 90.86% of JavaScript online submissions for Longest Substring Without Repeating Characters.
 */
- var lengthOfLongestSubstring = function(s) {
-     if(s.length === 0) return 0;
+var lengthOfLongestSubstring = function (s) {
+    if (s.length === 0) return 0;
 
-     let maxLength = -Infinity;
-     let substring = new Set();
-
-     for(let fastRunner = 0, slowRunner = 0; fastRunner < s.length; /* fastRunner++ */){
-        let currentLetter = s[fastRunner];
-        if(substring.has(currentLetter)){ //duplicate char found
-            slowRunner++;
-            fastRunner = slowRunner;
-            substring.clear();
+    let maxLength = -Infinity;
+    let set = new Set();
+    for (let left = 0, right = 0; right < s.length; right++) {
+        let currentLetter = s[right];
+        if (!set.has(currentLetter)) { //found letter not in set
+            set.add(currentLetter);
+            if (maxLength < right - left + 1) maxLength = right - left + 1;
         }
-        else{
-            substring.add(currentLetter);
-            if(substring.size > maxLength)
-                maxLength = substring.size;
-            fastRunner++;
-        }
-     }
 
-     return maxLength;
+        else { //we have a duplicate - iterate left until it moves past old duplicate
+            while (s[left] != currentLetter) {
+                set.delete(s[left]); //delete other letters along the way
+                left++;
+            }
+            left++; //we need to increment once more to move past duplicate
+        }
+    }
+
+    return maxLength;
 };
 
 (function test() {
+    console.log(lengthOfLongestSubstring("tmmzuxt"));
+    console.log(lengthOfLongestSubstring("dvdf"));
     console.log(lengthOfLongestSubstring("abcabcbb"));
-    console.log(lengthOfLongestSubstring("bbbbb"));
-    console.log(lengthOfLongestSubstring("pwwkew"));
-    console.log(lengthOfLongestSubstring(""));
-    console.log(lengthOfLongestSubstring("aab"));
+    // console.log(lengthOfLongestSubstring("bbbbb"));
+    // console.log(lengthOfLongestSubstring("pwwkew"));
+    // console.log(lengthOfLongestSubstring(""));
+    // console.log(lengthOfLongestSubstring("aab"));
 }())
