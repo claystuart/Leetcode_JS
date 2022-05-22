b51eeb9c4f53436c0c46a6bf66172345606f9749
Clay Stuart <clay.stuart@gmail.com>
2022-05-22 18:17:47 -0400

    Created RemoveDigitToMax.js (Leet #2259)

---
+++ RemoveDigitToMax.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/**
+ * @param {string} number
+ * @param {character} digit
+ * @return {string}
+ */
+ var removeDigit = function(number, digit) {
+    
+};
+
+(function(){
+    console.log("12 " + removeDigit("123", "3"));
+    console.log("231 " + removeDigit("1231", "1"));
+    console.log("51 " + removeDigit("551", "5"));
+})()
